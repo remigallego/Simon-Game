2a4133741ea32b09894493b1374424dbb9dbd2b7
remigallego <remi.gallego@gmail.com>
2018-01-29 15:28:26 +0100

    Commit #1

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+  /* Highlight a box */
+  function highlight(color) {
+  let box = "." + color;
+  let opacityTemp = $(box).css("opacity");
+  $(box).animate({
+    opacity: 1
+  }, 200, function() {
+    $(box).animate({
+      opacity: opacityTemp
+    }, 200);
+  });
+  }
+
+
+
+})
